8500efd9299c6de5b6aa8961eff71ec77168b3ea
ka2yuki <mori1103k@gmail.com>
2024-08-26 18:24:44 +0900

    ~結果を表示

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+2024年8月26日
+- Chromeエクステンションは 結構javascript読み込んでた。
+  - 検証 > Sources > Content scripts
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -9,11 +9,24 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true); // 手番の処理。先手“X”＝ture。
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true); // boolean. 手番の処理。先手“X”＝ture。
+  const [squares, setSquares] = useState(Array(9).fill(null)); // any. "X", "O", null
 
   function handleClick(i) {
-    if (squares[i]) {
+    /**
+     * 
+      > true || false
+      true
+      > false || true
+      true
+      > false || false
+      false
+      > false || null
+      null
+      > null || false
+      false
+     */
+    if (squares[i] || calculateWinner(squares)) { // false/null未選択の場合は右のコード勝利判定条件を実行。目視の場合は揃っているかどうか。
       return;
     }
     const nextSquares = squares.slice();
@@ -24,16 +37,32 @@ export default function Board() {
      * - 早期リターン (early return) 
      */
     if (xIsNext) {
-      nextSquares[i] = 'x';
+      nextSquares[i] = 'X';
     } else {
-      nextSquares[i] = 'o';
+      nextSquares[i] = 'O';
     }
     setSquares(nextSquares); // 盤面に反映
     setXIsNext(!xIsNext); // 反転してstate保存
   }
 
+  /**
+   * ゲームが終了したことを知らせる。
+   * - “Winner: X” または “Winner: O” というテキストを表示
+   *   - status に 結果をテキストstringで保持する。
+   * ゲームが続行中の場合は、次がどちらの手番なのか表示する。
+   */
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) { // “Winner: X” または “Winner: O” というテキストを表示
+    status = "Winner: " + winner;
+  } else { // ゲームが続行中の場合は、次がどちらの手番なのか表示する
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -51,4 +80,52 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+/**
+ * Return: string || null  
+ *   - 'X'、'O' または null  
+ * @param {*} squares
+ * 勝者が決まった際やこれ以上ゲームを進められなくなった際に、そのように表示する。
+ * - 実現するために、9 つのマス目の配列を受け取って勝者を判定し 'X'、'O' または null を返す。
+ * - calculateWinner という名前のヘルパー関数を追加。
+ */
+function calculateWinner(squares) {
+  /**
+   * - square配列のインデックスの場合の勝利条件を配列で用意する。square index: 0~8
+   * - 上記の配列をforで回す
+   *     - このゲーム判定は文字
+   *     - x or o < null
+   *     - if( この判定内容 )： 
+   *         - 文字の場合は後者が上書く
+   *           - b は a で上書く
+   *         - null:選択してない 場合は常に「選択してない」が上書く 
+   *         - つまり：配列squareを３つの配列の条件でindex世界においては
+   *             - 左側の[0,1,2,3,6]は真ん中の[1,3,4,5,7]を上書く
+   *             - 右側の[2,5,6,7,8]はそのまま 
+   *             - 結果は３つの値が全て同じ状況"x"もしくは"o"。で「左側」の位置の値が返却される。x・oはアイコン。
+    "x" && "x" 
+    > 'x'
+    "x" && "o" 
+    > 'o'
+    "x" && null 
+    > null
+   */
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]; // 勝利条件
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
